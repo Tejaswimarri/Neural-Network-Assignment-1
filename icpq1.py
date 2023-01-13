c501feec588b1cf8f47c4c78d84ddabbfc012b72
Tejaswimarri <122590257+Tejaswimarri@users.noreply.github.com>
2023-01-12 23:59:29 -0600

    Add files via upload

---
+++ icpq1.py
@@ -0,0 +1,5 @@
+string = input("Enter a string \n")
+del_string = string[1:-1]
+print ("Deleting first and last character of the string \n" + del_string)
+reversed_string = string[::-1]
+print("Reversed string is \n" +reversed_string)
---
+++ icpq3.py
@@ -0,0 +1,4 @@
+sentence = input("Enter a sentence \n")
+#replacing 'python' with 'pythons'
+replaced_sen = sentence.replace("python","pythons")
+print("Updated sentence is: \n"+replaced_sen )
